b678ac8706a2fe25cc8b61093a26da1ac83e70f3
qiuziZ <qiuzhi.zhu@shuyun.com>
2016-08-01 19:25:11 +0800

    ual2.0-upstream

--- src/app/upstream/upstreamCtrl.js
+++ src/app/upstream/upstreamCtrl.js
@@ -6,39 +6,77 @@ import {Inject} from 'angular-es-utils';
 import API from '../common/API/api.js';
 import services from '../common/service.js';
 
-@Inject('$scope', 'API', 'services')
+@Inject('API', 'services')
 class upstreamCtrl {
-	constructor($scope) {
+	constructor() {
 		let upstreamScope = this;
-		let api = upstreamScope._API;
-		upstreamScope.$scope = $scope;
-		upstreamScope.upstreamBalancing = '';
-		upstreamScope.upstreamName = '';
-		upstreamScope.upstreamNodes = '';
 		upstreamScope.getUpstreams();
+	}
+
+	initUpstream() {
+		let upstreamScope = this;
+		upstreamScope.upstreamItem = {
+			serviceName: '',
+			keepalive: '',
+			nodes: '',
+			balancing: ''
+		};
+		upstreamScope.nodeArray = [{
+			address: '',
+			port: '',
+			weight: ''
+		}];
+		upstreamScope.nodeArrayItem = [{
+			address: '',
+			port: '',
+			weight: ''
+		}];
 	}
 
 	getUpstreams() {
 		let upstreamScope = this;
 		let api = upstreamScope._API;
-		api.getUpstreams.get({}).$promise.then((data)=> {
+		upstreamScope.initUpstream();
+		api.getUpstreams.get({}, (data)=> {
 			upstreamScope.upstreams = data.upstreams;
-		}).catch();
+			angular.forEach(data.upstreams, function (item) {
+				item.nodeList = item.nodes.split(',');
+			})
+		});
 	}
 
 	addUpstream() {
 		let upstreamScope = this;
+		let list = '';
 		let api = upstreamScope._API;
 		let TipService = this._services.TipService;
-		api.upstream.save({
-			serviceName: upstreamScope.upstreamName,
-			nodes: upstreamScope.upstreamNodes,
-			balancing: upstreamScope.upstreamBalancing
-		}, (data) => {
+		angular.forEach(upstreamScope.nodeArrayItem, function (item, index) {
+			if (index == upstreamScope.nodeArray.length - 1) {
+				list += item.address + ':' + item.port + ':' + item.weight;
+			} else {
+				list += item.address + ':' + item.port + ':' + item.weight + ',';
+			}
+		});
+		upstreamScope.upstreamItem.nodes = list;
+		api.upstream.save({},
+			upstreamScope.upstreamItem,(data) => {
 			data.status ? TipService.setMessage('添加成功', 'success') : TipService.setMessage('添加失败', 'danger');
 			upstreamScope.getUpstreams();
 			$('#upstream').modal('hide');
 		})
+	}
+
+	addNode(){
+		let upstreamScope = this;
+		upstreamScope.nodeArrayItem.push({
+			address: '',
+			port: '',
+			weight: ''
+		});
+	}
+	popNode(index){
+		let upstreamScope = this;
+		upstreamScope.nodeArrayItem.splice(index,1);
 	}
 
 	delUpstream(upstreamId) {
@@ -56,8 +94,25 @@ class upstreamCtrl {
 
 	getUpstreamId(index, type) {
 		let upstreamScope = this;
+		upstreamScope.initUpstream();
 		upstreamScope.upstreamModal = upstreamScope.upstreams[index];
-		console.log('aaa:' + upstreamScope.upstreamModal);
+		if(upstreamScope.upstreamModal.nodeList.length > 1){
+			let len = upstreamScope.upstreamModal.nodeList.length;
+			for(let i = 1;i < len;i++){
+				upstreamScope.nodeArray.push({
+					address: '',
+					port: '',
+					weight: ''
+				});
+			}
+			angular.forEach(upstreamScope.upstreamModal.nodeList,function(item,index){
+				let nodeList = upstreamScope.upstreamModal.nodeList[index].split(':');
+				upstreamScope.nodeArray[index].address = nodeList[0];
+				upstreamScope.nodeArray[index].port = nodeList[1];
+				upstreamScope.nodeArray[index].weight = nodeList[2];
+			});
+		}
+
 		type == 'setting' ? $('#upstream-setting').modal('show') : $('#upstream-del').modal('show');
 	}
 
@@ -65,6 +120,15 @@ class upstreamCtrl {
 		let upstreamScope = this;
 		let api = upstreamScope._API;
 		let TipService = this._services.TipService;
+		let list = '';
+		angular.forEach(upstreamScope.nodeArray, function (item, index) {
+			if (index == upstreamScope.nodeArray.length - 1) {
+				list += item.address + ':' + item.port + ':' + item.weight;
+			} else {
+				list += item.address + ':' + item.port + ':' + item.weight + ',';
+			}
+		});
+		upstreamModal.nodes = list;
 		api.upstream.update({
 			upstreamId: upstreamModal.upstreamId,
 			serviceName: upstreamModal.serviceName,
