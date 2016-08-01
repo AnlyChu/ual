72cd526e2dd8c27e31d6c8319ed42dc59e0d0303
qiuziZ <qiuzhi.zhu@shuyun.com>
2016-08-01 16:03:43 +0800

    ual2.0-location

--- src/app/location/locationCtrl.js
+++ src/app/location/locationCtrl.js
@@ -8,18 +8,20 @@ class locationCtrl {
 	constructor() {
 		let locationScope = this;
 		let api = this._API;
-		locationScope.luasArray =[];
+		locationScope.luasArray = [];
 		locationScope.locationItem = {
 			urimatch: '',
 			internal: '',
 			rewrite: '',
 			upstreamId: '',
-			luaId: '',
+			luas: '',
 			defaultType: '',
 			excmd: '',
-			proxyConf:'',
+			auth: '',
+			dynamic_proxy: '',
+			proxyConf: '',
 			include: '',
-			locationName:''
+			locationName: ''
 		};
 		api.getUpstreams.get({}, (data) => {
 			locationScope.upstreams = data.upstreams;
@@ -34,25 +36,39 @@ class locationCtrl {
 
 	initLocationModal() {
 		let locationScope = this;
+		locationScope.luasArray = [];
 		locationScope.locationModal = {
 			urimatch: '',
 			internal: '',
 			rewrite: '',
 			upstream: {},
-			luas: {},
+			luas: '',
 			defaultType: '',
 			excmd: '',
-			proxyConf:'',
+			auth: '',
+			dynamic_proxy: '',
+			proxyConf: '',
 			include: '',
-			locationName:''
+			locationName: ''
 		}
 	}
 
 	getLocation() {
 		let locationScope = this;
 		let api = this._API;
+		locationScope.initLocationModal();
 		api.getLocations.get({}, (data) => {
 			locationScope.locations = data.locations;
+			angular.forEach(locationScope.locations, function (item) {
+				for (let i = 0, len = item.luas.length; i < len; i++) {
+					if (item.luas[i].type == 'auth') {
+						item.auth = item.luas[i].name;
+					}
+					if (item.luas[i].type == 'dynamic_proxy') {
+						item.dynamic_proxy = item.luas[i].name;
+					}
+				}
+			})
 		})
 	}
 
@@ -60,6 +76,7 @@ class locationCtrl {
 		let locationScope = this;
 		let api = this._API;
 		let TipService = this._services.TipService;
+		locationScope.locationItem.luas = locationScope.luasArray.join();
 		locationScope.locationItem.internal = locationScope.locationItem.internal === '' ? false : locationScope.locationItem.internal;
 		api.location.save({},
 			locationScope.locationItem, (data) => {
@@ -85,7 +102,23 @@ class locationCtrl {
 
 	getLocationId(index, type) {
 		let locationScope = this;
-		locationScope.locationModal = locationScope.locations[index];
+		locationScope.initLocationModal();
+		Object.assign(locationScope.locationModal, locationScope.locations[index]);
+		locationScope.locationModal.upstreamId = locationScope.locationModal.upstream.upstreamId;
+		if (locationScope.locationModal.auth) {
+			for (let i = 0, len = locationScope.locationModal.luas.length; i < len; i++) {
+				if (locationScope.locationModal.auth == locationScope.locationModal.luas[i].name) {
+					locationScope.luasArray[0] = locationScope.locationModal.luas[i].luaId.toString();
+				}
+			}
+		}
+		if (locationScope.locationModal.dynamic_proxy) {
+			for (let i = 0, len = locationScope.locationModal.luas.length; i < len; i++) {
+				if (locationScope.locationModal.dynamic_proxy == locationScope.locationModal.luas[i].name) {
+					locationScope.luasArray[1] = locationScope.locationModal.luas[i].luaId.toString();
+				}
+			}
+		}
 		type == 'setting' ? $('#location-setting').modal('show') : $('#location-del').modal('show');
 	}
 
@@ -93,6 +126,7 @@ class locationCtrl {
 		let locationScope = this;
 		let api = this._API;
 		let TipService = this._services.TipService;
+		locationModal.luas = locationScope.luasArray.join();
 		api.location.update({
 			urimatch: locationModal.urimatch,
 			internal: locationModal.internal,
@@ -100,9 +134,11 @@ class locationCtrl {
 			excmd: locationModal.excmd,
 			include: locationModal.include,
 			locationId: locationModal.locationId,
+			locationName: locationModal.locationName,
 			rewrite: locationModal.rewrite,
-			upstreamId: locationModal.upstream.upstreamId,
-			luaId: locationModal.lua.luaId
+			upstreamId: locationModal.upstreamId,
+			proxyConf: locationModal.proxyConf,
+			luas: locationModal.luas
 		}, (data) => {
 			locationScope.getLocation();
 			$('#location-setting').modal('hide');
@@ -113,6 +149,17 @@ class locationCtrl {
 			$('#location-setting').modal('hide');
 			TipService.setMessage('修改失败', 'danger');
 		})
+	}
+
+	onChangeLua() {
+		let locationScope = this;
+		if (locationScope.luasArray[1]) {
+			locationScope.haveDynamic = true;
+			locationScope.locationModal.upstreamId = '';
+		} else {
+			locationScope.haveDynamic = false;
+
+		}
 	}
 
 
