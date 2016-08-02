d43bfba0c0683ff36991cf45d38d2f3a4ea52c61
qiuziZ <qiuzhi.zhu@shuyun.com>
2016-08-02 11:51:49 +0800

    ual2.0-upstream

--- src/app/upstream/upstreamCtrl.js
+++ src/app/upstream/upstreamCtrl.js
@@ -59,85 +59,117 @@ class upstreamCtrl {
 		});
 		upstreamScope.upstreamItem.nodes = list;
 		api.upstream.save({},
-			upstreamScope.upstreamItem,(data) => {
-			data.status ? TipService.setMessage('添加成功', 'success') : TipService.setMessage('添加失败', 'danger');
-			upstreamScope.getUpstreams();
-			$('#upstream').modal('hide');
-		})
+			upstreamScope.upstreamItem, (data) => {
+				data.status ? TipService.setMessage('添加成功', 'success') : TipService.setMessage('添加失败', 'danger');
+				upstreamScope.getUpstreams();
+				$('#upstream').modal('hide');
+			})
 	}
 
-	addNode(){
+	addNode(type) {
 		let upstreamScope = this;
-		upstreamScope.nodeArrayItem.push({
-			address: '',
-			port: '',
-			weight: ''
-		});
-	}
-	popNode(index){
-		let upstreamScope = this;
-		upstreamScope.nodeArrayItem.splice(index,1);
+		if (type == 'add') {
+			upstreamScope.nodeArrayItem.push({
+				address: '',
+				port: '',
+				weight: ''
+			});
+		} else {
+			upstreamScope.nodeArray.push({
+				address: '',
+				port: '',
+				weight: ''
+			});
+		}
 	}
 
-	delUpstream(upstreamId) {
+	popNode(index, type) {
+		let upstreamScope = this;
+		type == 'add' ? upstreamScope.nodeArrayItem.splice(index, 1) : upstreamScope.nodeArray.splice(index, 1);
+	}
+
+	delUpstream(upstream, type) {
 		let upstreamScope = this;
 		let api = upstreamScope._API;
 		let TipService = this._services.TipService;
-		api.delUpstream.delete({
-			upstreamId: upstreamId
-		}, (data) => {
-			data.status ? TipService.setMessage('删除成功', 'success') : TipService.setMessage('删除失败', 'danger');
-			upstreamScope.getUpstreams();
-			$('#upstream-del').modal('hide');
-		})
+		if (type == 'del-node') {
+			upstreamScope.editUpstream(upstreamScope.nodes)
+		} else {
+			api.delUpstream.delete({
+				upstreamId: upstream.upstreamId
+			}, (data) => {
+				data.status ? TipService.setMessage('删除成功', 'success') : TipService.setMessage('删除失败', 'danger');
+				upstreamScope.getUpstreams();
+				$('#upstream-del').modal('hide');
+			})
+		}
 	}
 
-	getUpstreamId(index, type) {
+	getUpstreamId(index, type, nodeUpstream) {
 		let upstreamScope = this;
 		upstreamScope.initUpstream();
-		upstreamScope.upstreamModal = upstreamScope.upstreams[index];
-		if(upstreamScope.upstreamModal.nodeList.length > 1){
-			let len = upstreamScope.upstreamModal.nodeList.length;
-			for(let i = 1;i < len;i++){
-				upstreamScope.nodeArray.push({
-					address: '',
-					port: '',
-					weight: ''
+		upstreamScope.upstreamModal = angular.copy(upstreamScope.upstreams[index]);
+		if (type == 'setting') {
+			if (upstreamScope.upstreamModal.nodeList.length > 1) {
+				let len = upstreamScope.upstreamModal.nodeList.length;
+				for (let i = 1; i < len; i++) {
+					upstreamScope.nodeArray.push({
+						address: '',
+						port: '',
+						weight: ''
+					});
+				}
+				angular.forEach(upstreamScope.upstreamModal.nodeList, function (item, index) {
+					let nodeList = upstreamScope.upstreamModal.nodeList[index].split(':');
+					upstreamScope.nodeArray[index].address = nodeList[0];
+					upstreamScope.nodeArray[index].port = nodeList[1];
+					upstreamScope.nodeArray[index].weight = nodeList[2];
 				});
+			} else {
+				let nodeList = upstreamScope.upstreamModal.nodeList[0].split(':');
+				upstreamScope.nodeArray[0].address = nodeList[0];
+				upstreamScope.nodeArray[0].port = nodeList[1];
+				upstreamScope.nodeArray[0].weight = nodeList[2];
 			}
-			angular.forEach(upstreamScope.upstreamModal.nodeList,function(item,index){
-				let nodeList = upstreamScope.upstreamModal.nodeList[index].split(':');
-				upstreamScope.nodeArray[index].address = nodeList[0];
-				upstreamScope.nodeArray[index].port = nodeList[1];
-				upstreamScope.nodeArray[index].weight = nodeList[2];
-			});
+			$('#upstream-setting').modal('show')
+		} else if (type == 'del') {
+			$('#upstream-del').modal('show');
+		} else {
+			upstreamScope.upstreamModal = nodeUpstream;
+			upstreamScope.nodeUpstream = nodeUpstream.nodeList[index];
+			upstreamScope.nodes = angular.copy(nodeUpstream.nodeList).splice(index, 1);
+			$('#upstream-del-node').modal('show');
 		}
-
-		type == 'setting' ? $('#upstream-setting').modal('show') : $('#upstream-del').modal('show');
 	}
 
-	editUpstream(upstreamModal) {
+	editUpstream(nodes) {
 		let upstreamScope = this;
 		let api = upstreamScope._API;
 		let TipService = this._services.TipService;
 		let list = '';
-		angular.forEach(upstreamScope.nodeArray, function (item, index) {
-			if (index == upstreamScope.nodeArray.length - 1) {
-				list += item.address + ':' + item.port + ':' + item.weight;
-			} else {
-				list += item.address + ':' + item.port + ':' + item.weight + ',';
-			}
-		});
-		upstreamModal.nodes = list;
+		if (!nodes) {
+			angular.forEach(upstreamScope.nodeArray, function (item, index) {
+				if (index == upstreamScope.nodeArray.length - 1) {
+					list += item.address + ':' + item.port + ':' + item.weight;
+				} else {
+					list += item.address + ':' + item.port + ':' + item.weight + ',';
+				}
+			});
+			upstreamScope.upstreamModal.nodes = list;
+		} else {
+			upstreamScope.upstreamModal.nodes = nodes;
+		}
 		api.upstream.update({
-			upstreamId: upstreamModal.upstreamId,
-			serviceName: upstreamModal.serviceName,
-			nodes: upstreamModal.nodes,
-			balancing: upstreamModal.balancing
+			upstreamId: upstreamScope.upstreamModal.upstreamId,
+			serviceName: upstreamScope.upstreamModal.serviceName,
+			nodes: upstreamScope.upstreamModal.nodes,
+			balancing: upstreamScope.upstreamModal.balancing,
+			keepalive: upstreamScope.upstreamModal.keepalive
 		}, (data) => {
 			data.status ? TipService.setMessage('修改成功', 'success') : TipService.setMessage('修改失败', 'danger');
 			upstreamScope.getUpstreams();
 			$('#upstream-setting').modal('hide');
+			$('#upstream-del-node').modal('hide');
 		})
 	}
 }
